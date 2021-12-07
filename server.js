533b2f015df9b8441be386b1683a706dea68a0fc
Denva <emsley3684@gmail.com>
2021-12-07 12:26:18 -0500

    set up link and fetch requst o front end app

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json())
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     // Save personalityTraits as a dedicated array.
     let personalityTraitsArray = [];
@@ -96,6 +98,10 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
       res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
